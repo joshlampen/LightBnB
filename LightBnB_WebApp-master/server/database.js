92531f483aaabf28f23c73339c86680d1cd37032
JoshLampen <joshlampen2@gmail.com>
2020-05-27 18:17:53 +0000

    Converted getUserWithEmail(), getUserWithId(), and addUser() to use SQL queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -18,17 +18,15 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
+  const values = [email];
+  
+  return pool.query(`
+  SELECT *
+  FROM users
+  WHERE email = $1
+  `, values)
+  .then(res => res.rows[0] ? res.rows[0] : null);
+};
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -37,8 +35,15 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
-}
+  const values = [id];
+
+  return pool.query(`
+  SELECT *
+  FROM users
+  WHERE id = $1
+  `, values)
+  .then(res => res.rows[0] ? res.rows[0] : null);
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -48,11 +53,15 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+  const values = [user.name, user.email, user.password];
+
+  return pool.query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *
+  `, values)
+  .then(res => res.rows[0] ? res.rows[0] : null);
+};
 exports.addUser = addUser;
 
 /// Reservations
@@ -86,12 +95,6 @@ const getAllProperties = function(options, limit = 10) {
   LIMIT $1
   `, values)
   .then(res => res.rows);
-
-  // const limitedProperties = {};
-  // for (let i = 1; i <= limit; i++) {
-  //   limitedProperties[i] = properties[i];
-  // }
-  // return Promise.resolve(limitedProperties);
 };
 exports.getAllProperties = getAllProperties;
 
